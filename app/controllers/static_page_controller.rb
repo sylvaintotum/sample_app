deeb5589c89a9afa5d88d8a84e9ba92f88ddb650
sylvaintotum <sylvain@totumpass.com>
2012-07-11 12:38:26 -0400

    Exercice done

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -7,4 +7,8 @@ class StaticPageController < ApplicationController
 
   def about
   end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleAmm::Application.routes.draw do
   get "static_page/home"
   get "static_page/help"
   get "static_page/about"
+  get "static_page/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
