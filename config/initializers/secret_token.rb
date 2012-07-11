9a30cb8ffa6f6f3e36c0820693f6d8c04dfa1123
sylvaintotum <sylvain@totumpass.com>
2012-07-11 09:55:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAmm::Application.config.secret_token = 'bcefefde31a6e333e960e8debbdd81eeae51a056266e72ae69a9648d4352ab55cb020945a978600d28d12cd8b86206f1087d36050af3f458395f88cfa30a3a94'
